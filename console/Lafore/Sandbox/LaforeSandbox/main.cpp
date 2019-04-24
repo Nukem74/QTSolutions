d6aaa9a8875a51f02d4e61e92c27feac929a5334
Nukem74 <Nukem74@gmail.com>
2019-04-24 13:37:11 +0500

    Lafore sandbox

--- console/Lafore/Sandbox/LaforeSandbox/main.cpp
+++ console/Lafore/Sandbox/LaforeSandbox/main.cpp
@@ -1,25 +1,91 @@
-//example of stack
 #include <QCoreApplication>
 #include <iostream>
-#include <iomanip>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
-class Distance
+enum Suit {clubs, diamonds, hearts, spades};
+
+const int jack = 11;
+const int queen = 12;
+const int king = 13;
+const int ace = 14;
+
+class Card
 {
     private:
-        int feet;
-        float inches;
+        int number;
+        Suit suit;
     public:
-        void getdist()
+        Card()
         {
-            cout << "\n Enter feet: ";
-            cin >> feet;
-            cout << " Enter inches: ";
-            cin >> inches;
+            //empty
         }
-        void showdist()const
+        void set (int n, Suit s)
         {
-            cout << feet << "\'-" << inches << '\"';
+            suit = s;
+            number = n;
+        }
+        void display()
+        {   /*Отображение достоинства карт,
+            в том числе именованных при помощи
+            оператора switch*/
+
+            if( number >= 2 && number <= 10)
+            {
+                cout << number;
+            }
+            else
+            {
+                switch(number)
+                {
+
+                    case jack:
+                    {
+                        cout << 'J';
+                        break;
+                    }
+                    case queen:
+                    {
+                        cout << 'Q';
+                        break;
+                    }
+                    case king:
+                    {
+                        cout << 'K';
+                        break;
+                    }
+                    case ace:
+                    {
+                        cout << 'A';
+                        break;
+                    }
+                }
+            }
+            /*отображение масти карт*/
+            switch(suit)
+            {
+                case clubs:
+                {
+                    cout << static_cast<char>(5);
+                    break;
+                }
+                case diamonds:
+                {
+                    cout << static_cast<char>(4);
+                    break;
+                }
+                case hearts:
+                {
+                    cout << static_cast<char>(3);
+                    break;
+                }
+                case spades:
+                {
+                    cout << static_cast<char>(6);
+                    break;
+                }
+            }
         }
 };
 
@@ -28,25 +94,44 @@ class Distance
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    Distance dist [ 100 ];
-    int n = 0;
-    char ans;
+    Card deck[52];
+    int j;
 
     cout << endl;
+    for(j = 0;j < 52; j++)
+    {
+        int num = (j % 13) + 2;
+        Suit su = Suit(j/13);
+        deck[j].set(num, su);
+    }
+    cout << "Base deck: \n";
+    for(j = 0; j < 52; j++)
+    {
+        deck[j].display();
+        cout << "   ";
+        if(!((j + 1) % 13))
+        {
+            cout << endl;
+        }
+    }
+        srand(time(NULL));
+        for(j = 0; j < 52; j++)
+        {
+            int k = rand() % 52;
+            Card temp = deck[j];
+            deck[j] = deck [k];
+            deck[k] = temp;
+        }
+        cout << "Mixed deck: \n";
+        for(j = 0; j < 52; j++)
+        {
+            deck[j].display();
+            cout << "   ";
+            if(!((j + 1) % 13))
+            {
+                cout << endl;
+            }
+        }
 
-    do
-    {
-        cout << "Make new entry " << n + 1;
-        dist[n++].getdist();
-        cout << "Continue entry (y/n)?:";
-        cin >> ans;
-    }
-    while(ans != 'n');
-    for (int j = 0;j < n;j++)
-    {
-        cout << "\nEntry # " << j + 1 << " : ";
-        dist[j].showdist();
-    }
-    cout << endl;
     return a.exec();
 }
