7d5d15ec99c48cdb40c9f10aee2e224678560ac0
Nukem74 <Nukem74@gmail.com>
2019-04-25 10:19:41 +0500

    Lafore sandbox

--- console/Lafore/Sandbox/LaforeSandbox/main.cpp
+++ console/Lafore/Sandbox/LaforeSandbox/main.cpp
@@ -1,6 +1,6 @@
 #include <QCoreApplication>
 #include <iostream>
-#include <iomanip>
+#include <cstring>
 
 using namespace std;
 
@@ -8,11 +8,15 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    char str1[] = "Soft kitty, warm kitty - little ball of fur";
     const int MAX = 80;
-    char str [ MAX ];
-
-    cout << "Enter a string: ";
-    cin.get(str, MAX, '$');
-    cout << "Your entry: " << endl << str << endl;
+    char str2 [MAX];
+    int j;
+    for(j = 0; j < strlen(str1);j++)                //strlen возвращает длину строки
+    {
+        str2[j] = str1 [j];
+    }
+    str2[10] = '\0';                                // '\0' устанавливает символ окончания строки
+    cout << str2 << endl;
     return a.exec();
 }
