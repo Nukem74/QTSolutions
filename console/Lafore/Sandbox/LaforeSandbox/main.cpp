b1a449b25dda2ab2e68d48744b0b3e9b3a400daa
Nukem74 <Nukem74@gmail.com>
2019-04-25 10:23:35 +0500

    Lafore sandbox

--- console/Lafore/Sandbox/LaforeSandbox/main.cpp
+++ console/Lafore/Sandbox/LaforeSandbox/main.cpp
@@ -8,15 +8,10 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    char str1[] = "Soft kitty, warm kitty - little ball of fur";
+    char str1[] = "One ring to rule them all";
     const int MAX = 80;
-    char str2 [MAX];
-    int j;
-    for(j = 0; j < strlen(str1);j++)                //strlen возвращает длину строки
-    {
-        str2[j] = str1 [j];
-    }
-    str2[10] = '\0';                                // '\0' устанавливает символ окончания строки
+    char str2[MAX];
+    strcpy(str2, str1);                         //функция копирования строки
     cout << str2 << endl;
     return a.exec();
 }
