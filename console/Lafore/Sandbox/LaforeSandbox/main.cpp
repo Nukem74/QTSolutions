05f6fb1c339fee757a0671b3ef8378574f1d8c84
Nukem74 <Nukem74@gmail.com>
2019-04-25 10:44:54 +0500

    Lafore sandbox

--- console/Lafore/Sandbox/LaforeSandbox/main.cpp
+++ console/Lafore/Sandbox/LaforeSandbox/main.cpp
@@ -4,20 +4,35 @@
 
 using namespace std;
 
+class part
+{
+private:                        //перечисление приватных членов
+    char partname[30];
+    int partnumber;
+    double cost;
+public:                         //перечисление публичных членов
+    void setpart(char pname[], int pn, double c)    //метод, устанавливающий значения приватных полей, с тремя аргументами
+    {
+        strcpy(partname, pname);
+        partnumber = pn;
+        cost = c;
+    }
+    void showpart()             //метод без аргументов, отображающий значения приватных полей
+    {
+        cout << "\nName: " << partname;
+        cout << ". partnumber: " << partnumber;
+        cout << ". price :$" << cost;
+    }
+};
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    const int DAYS = 7;
-    const int MAX = 12;
-
-    char star[DAYS][MAX] =                      //объявление массива строк
-    {
-        "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
-    };
-    for(int j = 0; j < DAYS; j++)
-    {
-        cout << star[j] << endl;                //получение доступа к строке из массива строк выглядит как получение доступа к элементу одномерного массива, для этого используется первый индекс
-    }
+    part part1, part2;
+    part1.setpart("gear", 101, 0.11 );
+    part2.setpart("lever", 399, 1);
+    cout << "\nRecord #1: "; part1.showpart();
+    cout << "\nRecord #2: "; part2.showpart();
+    cout << endl;
     return a.exec();
 }
