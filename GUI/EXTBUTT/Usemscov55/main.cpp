350214658c17da0eeef8fffc03dcebc9a3d5022c
Nukem74 <Nukem74@gmail.com>
2019-04-26 14:29:56 +0500

    Usemscov55 done

--- GUI/EXTBUTT/Usemscov55/main.cpp
+++ GUI/EXTBUTT/Usemscov55/main.cpp
@@ -6,6 +6,10 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QPushButton exitButton(QString("Exit"));                                //создание объекта класса QPushButton
+    exitButton.setFont(QFont("Times", 50, QFont::Bold));                    //установка параметров шрифта надписи объекта exitButton
+    QObject::connect( &exitButton, SIGNAL(clicked()), &a, SLOT(quit()));     //соединение сигнала нажатия кнопки exitButton и слота quit приложения a
 
+    exitButton.show();
     return a.exec();
 }
