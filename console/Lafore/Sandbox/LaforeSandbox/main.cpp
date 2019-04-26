16a97acc9b86e1fcce828f11c888d8676ceb807b
Nukem74 <Nukem74@gmail.com>
2019-04-26 16:36:55 +0500

    Lafore sandbox

--- console/Lafore/Sandbox/LaforeSandbox/main.cpp
+++ console/Lafore/Sandbox/LaforeSandbox/main.cpp
@@ -1,5 +1,6 @@
 #include <QCoreApplication>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -8,22 +9,39 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    string s1("Let it go and calm down, since everyone's fine with it");
-    string s2("Fight");
-    string s3("hate");
-
-    s1.erase(0, 8);                         //удалить фрагмент из строки начинающий на позиции 0 и имеющий длину 8 символов
-    s1.replace(s1.find("fine"), 9, s3);     //заменить фрагмент в строке, позиция которого вычислена с помощью метода find, длинной 9 символов на значение переменной s3
-    s1.insert(0, s2);                       //вставить фрагмент в позицию 0б значение которого хранится в s2
-    s1.erase(s1.find("calm down"), 9);      //стереть фрагмент на позиции определенной с помощью метода find длинной 8 символов
-    s1.append(3, '!');                      //добавить в конце строки символ ! в количестве 3 штук
-
-    int x = s1.find(' ');
-    while(x < sizeof (s1))
+    string aName = "John";
+    string userName;
+    cout << "Enter your name: ";
+    getline(cin, userName);
+    if( userName == aName)
     {
-        s1.replace(x, 1, "/");              //заменить пробелы на левый слэш
-        x = s1.find(' ');
+        cout << "\nHello" << aName;
     }
-    cout << "s1: " << s1 << endl;
+    else if (userName < aName)
+    {
+        cout << "\nYour name before John";
+    }
+    else
+    {
+        cout << "\n Your name after John";
+    }
+
+    int n = userName.compare(0,2,aName, 0, 2);
+    cout << "\nFirst two letters of your name ";
+    if(n == 0)
+    {
+        cout << "match";
+    }
+    else if( n < 0 )
+    {
+        cout << "goes before ";
+    }
+    else
+    {
+        cout << "goes after ";
+    }
+    cout << aName.substr(0, 2) << endl;
+
+
     return a.exec();
 }
