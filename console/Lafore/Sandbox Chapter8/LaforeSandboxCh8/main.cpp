ea39f8a78ab8ea6ebfe3d79164b09b7d40289932
Nukem74 <Nukem74@gmail.com>
2019-04-29 16:46:54 +0500

    lafore sandbox ch8

--- console/Lafore/Sandbox Chapter8/LaforeSandboxCh8/main.cpp
+++ console/Lafore/Sandbox Chapter8/LaforeSandboxCh8/main.cpp
@@ -1,107 +1,55 @@
+//перегрузка оператора []
+//подготовка класса safearray
 #include <QCoreApplication>
 #include <iostream>
-
+#include <process.h>
 using namespace std;
 
-class Distance
+const int LIMIT = 100;                      //максимальный размер массива
+
+class safearray
 {
-private:
-    int feet;
-    float inches;
+private:                                    //приватное поле, содержащее целочисленный массив максимального размера
+    int arr[LIMIT];
 public:
-    Distance(): feet(0), inches(0.0)
+    void putel(int n, int elvalue)          //публичный метод с двумя аргументами и изменяющий значения элементов приватного массива
     {
-        //empty
+        if(n < 0 || n >= LIMIT)
+        {
+            {
+                cout << "\nWrong index!";
+                exit(1);
+            }
+
+        }
+        arr[n] = elvalue;
     }
-    Distance(int ft, float in): feet(ft), inches(in)
+    int getel(int n)const                   //публичный метод с одним аргументом возвращающий значение элемента приватного массива по его номеру
     {
-        //empty
+        if(n < 0 || n >= LIMIT)
+        {
+            {
+                cout << "\nWrong index!";
+                exit(1);
+            }
+
+        }
+        return arr[n];
     }
-    void getdist()
-    {
-        cout << "\nEnter feet: ";
-        cin >> feet;
-        cout << "\nEnter inches: ";
-        cin >> inches;
-    }
-    void showdist()
-    {
-        cout << feet << "\'-" << inches << '\"';
-    }
-    Distance operator + (Distance)const;            //переопределение операторов
-    bool operator < (Distance) const;
-    void operator += (Distance);              //внимание! Лафоре в своем примере по переопределению оператора "+=" объявляет этот оператор постоянным! это недопустимо, так как действие оператора подразумевает изменение приватных полей объекта его вызывающего
 };
-
-void Distance::operator+=(Distance d2)
-{
-    feet += d2.feet;
-    inches += d2.inches;
-    if( inches >= 12.0)
-    {
-         inches -= 12.0;
-         feet++;
-    }
-}
-
-bool Distance::operator < (Distance d2) const
-{
-    float bf1 = feet + inches / 12;
-    float bf2 = d2.feet + d2.inches / 12;
-    return (bf1 < bf2) ? true : false;
-}
-/*
-Переопределенный оператор "<" вызывается объектом класса Distance
-принимает в себя другой объект класса Distance
-и возвращает значение типа bool
-*/
-
-Distance Distance::operator+( Distance d2) const        //переопределение бинарного оператора сложения
-{
-    int f = feet +d2.feet;
-    float i = inches + d2.inches;
-    if( i >= 12.0)
-    {
-         i -= 12.0;
-         f++;
-    }
-    return Distance(f, i);
-}
-
-/*
-Переопределенный оператор "+" вызывается объектом класса Distance,
-принимает в себя другой объект класса Distance
-и возвращает третий -  безымянный объект класса Distance
-*/
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    Distance dist1, dist3, dist4;
-    dist1.getdist();
-    Distance dist2(11, 6.25);
-    dist3 = dist1 + dist2;
-    dist4 += dist3;
-
-    cout << "dist1 = ";
-    dist1.showdist();
-    cout << endl;
-    cout << "dist2 = ";
-    dist2.showdist();
-    cout << endl;
-    cout << "dist3 = ";
-    dist3.showdist();
-    cout << endl;
-    cout << "dist4 = ";
-    dist4.showdist();
-    cout << endl;
-    if(dist1 < dist2)
-        cout << "\ndist1 lesser then dist2";
-    else
+    safearray sal;                      //инициализация массива
+    for(int i = 0; i < LIMIT; i++)
     {
-        cout << "\ndist1 larger then dist2";
+        sal.putel(i, i * 10);           //заполнение массива
     }
-    cout << endl;
-
+    for (int i = 0; i < LIMIT;i++)
+    {
+        int temp = sal.getel(i);
+        cout << "Element " << i << "is" << temp << endl;        //отображение массива
+    }
     return a.exec();
 }
