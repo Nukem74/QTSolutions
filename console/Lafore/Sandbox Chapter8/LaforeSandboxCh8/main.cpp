7d18c479fc2b542bdc2a650a76095eca6bc87204
Nukem74 <Nukem74@gmail.com>
2019-04-30 08:25:48 +0500

    lafore sandbox ch8

--- console/Lafore/Sandbox Chapter8/LaforeSandboxCh8/main.cpp
+++ console/Lafore/Sandbox Chapter8/LaforeSandboxCh8/main.cpp
@@ -3,32 +3,7 @@
 #include <string>
 using namespace std;
 
-class time12
-{
-private:
-    bool pm;
-    int hrs;
-    int mins;
-public:
-    time12():pm(true), hrs(0),mins(0)
-    {
-        //empty
-    }
-    time12(bool ap, int h, int m): pm(ap), hrs(h), mins(m)
-    {
-        //empty
-    }
-    void display()const
-    {
-        cout << hrs << ':';
-        if(mins<10)
-            cout << '0';
-        cout << mins << ' ';
 
-        string am_pm = pm ? "p.m." : "a.m.";
-        cout << am_pm;
-    }
-};
 
 class time24
 {
@@ -64,9 +39,53 @@ public:
         cout << seconds << endl;
 
     }
-    operator time12() const;
+    //operator time12() const;
+    int getHrs()const
+    {
+        return hours;
+    }
+    int getMins()const
+    {
+        return minutes;
+    }
+    int getSecs()const
+    {
+        return seconds;
+    }
 };
-time24::operator time12() const
+
+
+
+
+class time12
+{
+private:
+    bool pm;
+    int hrs;
+    int mins;
+public:
+    time12():pm(true), hrs(0),mins(0)
+    {
+        //empty
+    }
+    time12(bool ap, int h, int m): pm(ap), hrs(h), mins(m)
+    {
+        //empty
+    }
+    time12(time24);
+    void display()const
+    {
+        cout << hrs << ':';
+        if(mins<10)
+            cout << '0';
+        cout << mins << ' ';
+
+        string am_pm = pm ? "p.m." : "a.m.";
+        cout << am_pm;
+    }
+};
+
+/*time24::operator time12() const
 {
     int hrs24 = hours;
     bool pm = (hours < 12) ? false : true;
@@ -85,6 +104,27 @@ time24::operator time12() const
         pm = false;
     }
     return time12(pm, hrs12, roundMins);
+}*/
+
+time12::time12(time24 t24)                  //конструктор преобразования
+{
+    int hrs24 = t24.getHrs();
+    pm = t24.getHrs() < 12 ? t24.getMins() : t24.getMins() + 1;
+    if(mins == 60)
+    {
+        mins = 0;
+        ++hrs24;
+        if(hrs24 ==12 || hrs24 == 24)
+        {
+            pm = (pm == true) ? false : true;
+        }
+    }
+    hrs = (hrs24 < 13) ? hrs24 : hrs24 - 12;
+    if(hrs == 0)
+    {
+        hrs = 12;
+        pm = false;
+    }
 }
 
 int main(int argc, char *argv[])
