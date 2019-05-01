b47760cfacb399e59cf6648a4f6cfbedafbba16d
Nukem74 <Nukem74@gmail.com>
2019-05-01 09:39:07 +0500

    lafore sandbox ch9

--- console/Lafore/Sandbox Chapter9/LaforeSandboxCh9/main.cpp
+++ console/Lafore/Sandbox Chapter9/LaforeSandboxCh9/main.cpp
@@ -3,44 +3,117 @@
 #include <process.h>
 using namespace std;
 
-class A                 //объявление класса
+const int LEN = 80;
+
+class employee                                  //объявление класса
 {
-private:                //приватный член
-    int privadataA;
-protected:              //защищенный член
-    int protdataA;
-public:                 //публичный член
-    int pubdataA;
-};
-class B: public A       //объявление класса-наследника с доступом к публичным членам предка
-{
-public:
-    void func()
+private:                                        //перечисление приватных членов
+    char name[LEN];
+    unsigned long number;
+public:                                         //перечисление публичных членов
+    void getdata()
     {
-        int a;
-        //a = privadataA;
-        a = protdataA;
-        a = pubdataA;
+        cout << "\n State second name: ";
+        cin >> name;
+        cout << "\n State ID: ";
+        cin >> number;
+    }
+    void putdata ()const
+    {
+        cout << "\n Second name: " << name;
+        cout << "\nID: " << number;
     }
 };
-class C: private A      //объявление класса-наследника без досту
-{
-public:
-    void func()
-    {
-        int a;
-        //a = privadataA;
-        a = protdataA;
-        a = pubdataA;
 
+class manager : public employee                 //объявление класса-наследника с доступом к публичным членам предка
+{
+private:                                        //перечисление приватных членов
+    char title[LEN];
+    double dues;
+public:
+    void getdata()                              //перегрузка метода базового класса
+    {
+        cout << "\nManagement staff";
+        employee::getdata();
+        cout << "\n State title: ";
+        cin >> title;
+        cout << "\n Designate dues: ";
+        cin >> dues;
+    }
+    void putdata()const                         //перегрузка метода базового класса
+    {
+        employee::putdata();
+        cout << "\n Title: " << title;
+        cout << "\n Golf-club dues paid: " << dues;
+    }
+
+};
+class scientist:public employee                 //объявление класса-наследника с доступом к публичным членам предка
+{
+private:                                        //перечисление приватных членов
+    int pubs;
+public:                                         //перечисление публичных членов
+    void getdata()                              //перегрузка метода базового класса
+    {
+        cout << "\nDeveloping staff";
+        employee::getdata();
+        cout << "\n Designate publications index: ";
+        cin >> pubs;
+    }
+    void putdata()const                         //перегрузка метода базового класса
+    {
+        employee::putdata();
+        cout << "\n Publications index: " << pubs;
+    }
+};
+
+class laborer: public employee                  //объявление класса-наследника с доступом к публичным членам предка
+{
+public:                                         //перечисление публичных членов
+    void getdata()                              //перегрузка метода базового класса
+    {
+        cout << "\nCOMRADE";
+        employee::getdata();
+    }/*Empty inherited class for laborer?! SJW triggered*/
+};
+
+class foreman: public laborer                   //объявление класса-наследника второго порядка с доступом к публичным членам предка
+{
+private:
+    float quotas;
+public:
+    void getdata()
+    {
+
+        laborer::getdata();
+        cout << " FOREMAN";
+        cout << "/nDesignate quote: ";
+        cin >> quotas;
+    }
+    void putdata() const
+    {
+        laborer::putdata();
+        cout << "\n Quote: ";
     }
 };
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    manager m1, m2;
+    scientist s1;
+    laborer l1;
+    foreman f1;
 
-
-    /*QT не даст мне скомпилировать программу из-за очевидных ошибок*/
+    m1.getdata();
+    m2.getdata();
+    s1.getdata();
+    l1.getdata();
+    f1.getdata();
+    m1.putdata();
+    m2.putdata();
+    f1.putdata();
+    l1.putdata();
+    s1.putdata();
     return a.exec();
 }
