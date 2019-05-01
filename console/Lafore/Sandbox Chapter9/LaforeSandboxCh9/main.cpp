4c5771336d62b8914c32e439460c1df164ce49ef
Nukem74 <Nukem74@gmail.com>
2019-05-01 09:18:20 +0500

    lafore sandbox ch9

--- console/Lafore/Sandbox Chapter9/LaforeSandboxCh9/main.cpp
+++ console/Lafore/Sandbox Chapter9/LaforeSandboxCh9/main.cpp
@@ -3,94 +3,44 @@
 #include <process.h>
 using namespace std;
 
-const int LEN = 80;
-
-class employee                                  //объявление класса
+class A                 //объявление класса
 {
-private:                                        //перечисление приватных членов
-    char name[LEN];
-    unsigned long number;
-public:                                         //перечисление публичных членов
-    void getdata()
-    {
-        cout << "\n State sescond name: ";
-        cin >> name;
-        cout << "\n State ID: ";
-        cin >> number;
-    }
-    void putdata ()const
-    {
-        cout << "\n Second name: " << name;
-        cout << "\nID: " << number;
-    }
+private:                //приватный член
+    int privadataA;
+protected:              //защищенный член
+    int protdataA;
+public:                 //публичный член
+    int pubdataA;
 };
-
-class manager : public employee                 //объявление наследуемого класса с доступом к публичным членам предка
+class B: public A       //объявление класса-наследника с доступом к публичным членам предка
 {
-private:                                        //перечисление приватных членов
-    char title[LEN];
-    double dues;
 public:
-    void getdata()                              //перегрузка метода базового класса
+    void func()
     {
-        cout << "\nManagement staff";
-        employee::getdata();
-        cout << "\n State title: ";
-        cin >> title;
-        cout << "\n Designate dues: ";
-        cin >> dues;
-    }
-    void putdata()const                         //перегрузка метода базового класса
-    {
-        employee::putdata();
-        cout << "\n Title: " << title;
-        cout << "\n Golf-club dues paid: " << dues;
-    }
-
-};
-class scientist:public employee                 //объявление наследуемого класса с доступом к публичным членам предка
-{
-private:                                        //перечисление приватных членов
-    int pubs;
-public:                                         //перечисление публичных членов
-    void getdata()                              //перегрузка метода базового класса
-    {
-        cout << "\nDeveloping staff";
-        employee::getdata();
-        cout << "\n Designate publications index: ";
-        cin >> pubs;
-    }
-    void putdata()const                         //перегрузка метода базового класса
-    {
-        employee::putdata();
-        cout << "\n Publications index: " << pubs;
+        int a;
+        //a = privadataA;
+        a = protdataA;
+        a = pubdataA;
     }
 };
-
-class laborer: public employee                  //объявление наследуемого класса с доступом к публичным членам предка
+class C: private A      //объявление класса-наследника без досту
 {
-public:                                         //перечисление публичных членов
-    void getdata()                              //перегрузка метода базового класса
+public:
+    void func()
     {
-        cout << "\nCOMRADE";
-        employee::getdata();
-    }/*Empty inherited class for laborer?! SJW triggered*/
+        int a;
+        //a = privadataA;
+        a = protdataA;
+        a = pubdataA;
+
+    }
 };
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    manager m1, m2;
-    scientist s1;
-    laborer l1;
 
-    m1.getdata();
-    m2.getdata();
-    s1.getdata();
-    l1.getdata();
-    m1.putdata();
-    m2.putdata();
-    l1.putdata();
-    s1.putdata();
+
+    /*QT не даст мне скомпилировать программу из-за очевидных ошибок*/
     return a.exec();
 }
