c9a7d3bc50270947f93f1dd0c5b054b912978370
Nukem74 <Nukem74@gmail.com>
2019-05-06 12:35:43 +0500

    Lafore sandbox ch10 PTRVAR

--- console/Lafore/SandboxChapter10/SandboxCh10/main.cpp
+++ console/Lafore/SandboxChapter10/SandboxCh10/main.cpp
@@ -6,10 +6,13 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
     int var1 = 11;
     int var2 = 22;
-    int var3 = 33;
 
-    cout << &var1 << endl;
-    cout << &var2 << endl;
-    cout << &var3 << endl;
+    cout << &var1 << endl << &var2 << endl << endl;
+
+    int *ptr;                       //создание указателя на переменную типа int
+    ptr = &var1;                    //присвоение указателю адреса переменной
+    cout << ptr << endl;
+    ptr = &var2;                    //присвоение указателю адреса переменной
+    cout << ptr << endl;
     return a.exec();
 }
