11f787266ba606cad508f4683ce78ce04bdf62a2
Nukem74 <Nukem74@gmail.com>
2019-05-06 13:01:44 +0500

    Lafore sandbox ch10 PTRACC

--- console/Lafore/SandboxChapter10/SandboxCh10/main.cpp
+++ console/Lafore/SandboxChapter10/SandboxCh10/main.cpp
@@ -7,12 +7,13 @@ int main(int argc, char *argv[])
     int var1 = 11;
     int var2 = 22;
 
-    cout << &var1 << endl << &var2 << endl << endl;
-
     int *ptr;                       //создание указателя на переменную типа int
     ptr = &var1;                    //присвоение указателю адреса переменной
-    cout << ptr << endl;
+    //отображение адреса, и значения переменной, хранящейся по адресу
+    cout << "Variable, stored at "<< ptr <<" has value: "<< *ptr << endl;
+
     ptr = &var2;                    //присвоение указателю адреса переменной
-    cout << ptr << endl;
+    //отображение адреса, и значения переменной, хранящейся по адресу
+    cout << "Variable, stored at "<< ptr <<" has value: "<< *ptr << endl;
     return a.exec();
 }
