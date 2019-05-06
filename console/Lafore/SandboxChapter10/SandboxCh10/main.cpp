c19ada0f10d95dff2d22a48a96dabe54c5e35d2c
Nukem74 <Nukem74@gmail.com>
2019-05-06 13:48:41 +0500

    Lafore sandbox ch10 passreff

--- console/Lafore/SandboxChapter10/SandboxCh10/main.cpp
+++ console/Lafore/SandboxChapter10/SandboxCh10/main.cpp
@@ -1,16 +1,22 @@
 #include <QCoreApplication>
 #include <iostream>
 using namespace std;
+
+void centimize(double& V)
+{
+    V*= 2.54;
+}
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    int intarray [5] = {31, 54, 77, 52, 93};    //объявление переменной
-    int* ptrint;                                //объявление указателя
-    ptrint = intarray;                          //присвоение указателю адреса массива
-    for(int i = 0; i < 5; i++)
-    {
-        cout << *(ptrint++) << endl;            //доступ к элементам массива с помощью указателя типа int, прирастающего на величину памяти выделяемой для типа int
-    }
+    void centimize (double&);
+    double var = 10.0;
+    cout << "var = " << var << "inches" << endl;
+
+    centimize(var);
+    cout << "var = " << var << "centimeters" << endl;
 
     return a.exec();
 }
+
+
