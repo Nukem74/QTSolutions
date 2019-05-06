452fa3bb9cdd237cd68c27ba7e584e5507a927ab
Nukem74 <Nukem74@gmail.com>
2019-05-06 13:51:23 +0500

    Lafore sandbox ch10 passptr

--- console/Lafore/SandboxChapter10/SandboxCh10/main.cpp
+++ console/Lafore/SandboxChapter10/SandboxCh10/main.cpp
@@ -2,18 +2,21 @@
 #include <iostream>
 using namespace std;
 
-void centimize(double& V)
+void centimize(double *ptrd)
 {
-    V*= 2.54;
+    *ptrd*= 2.54;
 }
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    void centimize (double&);
+
+    void centimize (double*);
+
     double var = 10.0;
+
     cout << "var = " << var << "inches" << endl;
 
-    centimize(var);
+    centimize(&var);
     cout << "var = " << var << "centimeters" << endl;
 
     return a.exec();
