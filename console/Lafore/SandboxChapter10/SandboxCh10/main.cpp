305cf16458c75c5e6e1cbdf19f676541b318a1df
Nukem74 <Nukem74@gmail.com>
2019-05-07 07:29:36 +0500

    Lafore sandbox ch10 passarr

--- console/Lafore/SandboxChapter10/SandboxCh10/main.cpp
+++ console/Lafore/SandboxChapter10/SandboxCh10/main.cpp
@@ -2,23 +2,23 @@
 #include <iostream>
 using namespace std;
 
+const int MAX = 5;
+
 void centimize(double *ptrd)
 {
-    *ptrd*= 2.54;
+    for (int i = 0; i < MAX; i ++)
+    {
+        *ptrd++ *= 2.54;
+    }
 }
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
-    void centimize (double*);
-
-    double var = 10.0;
-
-    cout << "var = " << var << "inches" << endl;
-
-    centimize(&var);
-    cout << "var = " << var << "centimeters" << endl;
-
+    void centimize(double*);
+    double varray[MAX] = {10.0, 43.1 , 95.9, 58.7, 87.3};
+    centimize(varray);
+    for(int j = 0; j < MAX; j++)
+        cout << "varray [ " << j << " ] = " << varray[j] << "centimeters" << endl;
     return a.exec();
 }
 
