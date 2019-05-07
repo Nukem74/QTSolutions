66fd709c0c0d7db74fe213c239666467d7117afa
Nukem74 <Nukem74@gmail.com>
2019-05-07 07:45:27 +0500

    Lafore sandbox ch10 passarr

--- console/Lafore/SandboxChapter10/SandboxCh10/main.cpp
+++ console/Lafore/SandboxChapter10/SandboxCh10/main.cpp
@@ -4,22 +4,25 @@ using namespace std;
 
 const int MAX = 5;
 
-void centimize(double *ptrd)
-{
-    for (int i = 0; i < MAX; i ++)
-    {
-        *ptrd++ *= 2.54;
-    }
-}
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     void centimize(double*);
     double varray[MAX] = {10.0, 43.1 , 95.9, 58.7, 87.3};
-    centimize(varray);
+    centimize(varray);                  //имя массива - адресс, поэтому не нужно использовать операцию & взятия адреса
     for(int j = 0; j < MAX; j++)
         cout << "varray [ " << j << " ] = " << varray[j] << "centimeters" << endl;
     return a.exec();
 }
 
-
+void centimize(double *ptrd)            //функция с одним аргументом - указателем на тип double
+{
+    for (int i = 0; i < MAX; i ++)
+    {   //унарные операторы обладают правой ассоциативностью
+        //то есть сначала выполняется инкремент
+        //а затем разыменование(обращение по адресу)
+        //лучше я не буду использовать такие конструкции...
+        *ptrd++ *= 2.54;                //присвоить адресу, приращенному на величину типа double, значение элемента массива домноженное на константу
+    }
+}
