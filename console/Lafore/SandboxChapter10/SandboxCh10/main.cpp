1c67643c2a370e3a0b6402637071d859e747043d
Nukem74 <Nukem74@gmail.com>
2019-05-08 07:37:38 +0500

    Lafore sandbox ch10 NEWSTR

--- console/Lafore/SandboxChapter10/SandboxCh10/main.cpp
+++ console/Lafore/SandboxChapter10/SandboxCh10/main.cpp
@@ -4,18 +4,39 @@
 using namespace std;
 
 
+class String
+{
+private:                                    //перечисление приватных членов
+    char* str;                              //объявление указателя на тип char
+public:                                     //перечисление публичных членов
+    String(char* s)                         //конструктор с одним аргументом-указателем на тип char
+    {
+        //int length = strlen(s);
+        str = new char [strlen(s) + 1];     //выделение памяти, необходимое для хранения передаваемой конструктору переменной
+        strcpy(str, s);
+    }
+    ~String()                               //деструктор
+    {
+        cout << "String deleted";
+        delete [] str;
+    }
+    void display()                          //публичный метод отображающий содержимое на которое указывает приватный указатель
+    {
+        cout << str << endl;
+    }
+};
+/*
+класс String имеет только одно поле str, являющееся указателем на char.
+Он будет указывать на строку, содержащуюся в объекте класса String.
+Здесь не будет массива, содержащего строку. Строка хранится в другом
+месте, а в классе String есть только указатель на нее.
+*/
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    char* str = "No rest for the wicked";       //объявление указателя на тип char
-    //int len = strlen(str);
-
-    char* ptr = new char [strlen(str) + 1];     //определение указателя на тип char с выделением памяти, равно длине строки str
-                                                //но не ясно, что я приобретаю от выделения памяти. Как увидеть преимущество этого приема?
-    strcpy(ptr, str);
-    cout << "ptr = " << ptr << endl;
-
-    delete [] ptr;
+    String s1 = "High speed - low drag";
+    cout << "s1 = ";
+    s1.display();
     return a.exec();
 }
 
