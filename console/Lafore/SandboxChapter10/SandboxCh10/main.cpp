514a656d36db0ea2297a35158cefbd6d404c63d0
Nukem74 <Nukem74@gmail.com>
2019-05-08 07:53:28 +0500

    Lafore sandbox ch10 ENGLPTR

--- console/Lafore/SandboxChapter10/SandboxCh10/main.cpp
+++ console/Lafore/SandboxChapter10/SandboxCh10/main.cpp
@@ -3,40 +3,37 @@
 #include <cstring>
 using namespace std;
 
-
-class String
+class Distance                                      //объявление класса
 {
-private:                                    //перечисление приватных членов
-    char* str;                              //объявление указателя на тип char
-public:                                     //перечисление публичных членов
-    String(char* s)                         //конструктор с одним аргументом-указателем на тип char
+private:                                            //перечисление приватных членов
+    int feet;
+    float inches;
+public:                                             //перечисление публичных членов
+    void getdist()                                  //публичный метод изменяющий значения приватных полей
     {
-        //int length = strlen(s);
-        str = new char [strlen(s) + 1];     //выделение памяти, необходимое для хранения передаваемой конструктору переменной
-        strcpy(str, s);
+        cout << "\nEnter feet: ";
+        cin >> feet;
+        cout << "\nEnter inches: ";
+        cin >> inches;
     }
-    ~String()                               //деструктор
+    void showdist()                                 //публичный метод отображающий значение приватных полей
     {
-        cout << "String deleted";
-        delete [] str;
-    }
-    void display()                          //публичный метод отображающий содержимое на которое указывает приватный указатель
-    {
-        cout << str << endl;
+        cout << feet << "\'-" << inches << '\"';
     }
 };
-/*
-класс String имеет только одно поле str, являющееся указателем на char.
-Он будет указывать на строку, содержащуюся в объекте класса String.
-Здесь не будет массива, содержащего строку. Строка хранится в другом
-месте, а в классе String есть только указатель на нее.
-*/
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    String s1 = "High speed - low drag";
-    cout << "s1 = ";
-    s1.display();
+    Distance dist;                                  //объявление объекта
+    dist.getdist();
+    dist.showdist();
+    Distance* distptr = new Distance;               //объявление указателя на объект
+
+    distptr->getdist();                             //вызов метода от разыменованного указателя на объект
+    //тоже, что(*distptr).getdist();
+    distptr->showdist();                            //вызов метода от разыменованного указателя на объект
+    //тоже, что (*distptr).showdist;
     return a.exec();
 }
 
