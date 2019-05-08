1ce400209b298491cdf9d1f436941ff0f98febf0
Nukem74 <Nukem74@gmail.com>
2019-05-08 06:54:35 +0500

    Lafore sandbox ch10 PTROSTR

--- console/Lafore/SandboxChapter10/SandboxCh10/main.cpp
+++ console/Lafore/SandboxChapter10/SandboxCh10/main.cpp
@@ -6,10 +6,12 @@ const int DAYS = 7;
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    char* arrptrs[DAYS] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+    char* arrptrs[DAYS] = {"Monday", "Tuesday", "Wednesday",
+                           "Thursday", "Friday", "Saturday",
+                           "Sunday"};                   //объявление массива указателей на тип char
     for(int i = 0; i < DAYS; i++)
     {
-        cout << &arrptrs[i] << arrptrs[i] << endl;
+        cout << &arrptrs[i] << arrptrs[i] << endl;      //отображение элементов массива указателей и их адресов
     }
     return a.exec();
 }
