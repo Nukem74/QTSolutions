4b29ce1b5d9ec24f5aafa7efc304fb1757258864
Nukem74 <Nukem74@gmail.com>
2019-05-13 06:04:49 +0500

    Lafore sandbox ch10 endoffile

--- console/Lafore/SandboxChapter10/SandboxCh10/main.cpp
+++ console/Lafore/SandboxChapter10/SandboxCh10/main.cpp
@@ -3,45 +3,98 @@
 #include <cstring>
 using namespace std;
 
-class person                                        //объявление класса
+struct link                         //элемент списка
 {
-private:                                            //перечисление приватных членов
-    char name [40];
-public:                                             //перечисление публичных членов
-    void setName()                                  //публичный метод без аргументов
+    int data;                       //целочисленное содержимое элемента
+    link* next;                     //указатель на тип link
+};
+
+class linklist                      //объявление класса
+{
+private:                            //перечисление приватных членов
+    link* first;
+public:                             //перечисление публичных членов
+    linklist()                      //конструктор без аргументов
     {
-        cout << "Enter name: ";
-        cin >> name;
+        first = NULL;
     }
-    void printName()                                //публичный метод без аргументов
+
+    void addItem(int d)             //публичный метод с одним аргументом
     {
-        cout << "\n Name: " << name;
+        link* newlink = new link;   //создание экземпляра link по адресу указателя newlink
+        newlink->data = d;          //изменение содержимого link по адресу указателя
+        newlink->next = first;      //изменение элемента списка
+        first = newlink;
     }
+
+    /*void remList()
+    {
+        link* current = first;
+        link* temp = current;
+        do
+        {
+            temp = current->next;
+            current = NULL;
+            cout << current->data << endl;
+            current = temp;
+            cout << current->data << endl;
+            cout << endl;
+        }
+        while(current);
+    }*/
+
+    void display()                  // публичный метод без аргументов
+    {
+        link* current = first;
+        while(current)
+        {
+            cout << current->data << endl;
+            current = current->next;
+        }
+    }
+    void display(int n)                  // публичный метод без аргументов
+    {
+        link* current = first;
+        while(current)
+        {
+            if(current->data == n)
+            {
+                cout << current->data << endl;
+            }
+            current = current->next;
+        }
+    }
+    void remItem(int n)
+    {
+        link* current = first;
+        while(current)
+        {
+            if(current->data == n)
+            {
+                delete current;
+            }
+            current = current->next;
+        }
+
+     }
 };
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    person* persPtr [100];                          //определение массива указателей на объекты
-    int n = 0;                                      //счетчик объектов
-    char choice;
-    do
-    {
-        persPtr[n] = new person;                    //создание нового объекта класса person по адресу из массива указателей
-        persPtr[n]->setName();                      //изменение приватного поля объекта по адресу из массива указателей
-        n++;                                        //инкремент счетчика объектов в массиве указателей
-        cout << "Continue(y/n)?";
-        cin >> choice;
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    linklist li;
 
-    }
-    while(choice == 'y');
-    for(int i = 0; i < n; i++)
-    {
-        cout << "\nInfo #" << i + 1;
-        persPtr[i]->printName();                    //отображение содержимого приватного поля объекта по адресу из массива указателей
-    }
-    cout << endl;
+    li.addItem(25);
+    li.addItem(36);
+    li.addItem(49);
+    li.addItem(64);
+
+    li.remItem(49);
+
+    li.display();
+
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     return a.exec();
 }
