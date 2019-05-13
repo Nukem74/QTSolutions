c513cedf90b388880f3c8e45a6f1ce41acdb5386
Nukem74 <Nukem74@gmail.com>
2019-05-13 07:02:10 +0500

    Usemscov 513 basic

--- GUI/USemscov513/USemscov513/main.cpp
+++ GUI/USemscov513/USemscov513/main.cpp
@@ -1,11 +1,23 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include "mywin.h"
+
+MyWin::MyWin(const QString& title) : QMainWindow (0)
+{
+
+    setWindowTitle(title);
+    resize(200, 120);
+    move(150, 100);
+
+}
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+
+    MyWin win ("My window");
+    win.show();
 
     return a.exec();
 }
--- GUI/USemscov513/USemscov513/mywin.h
+++ GUI/USemscov513/USemscov513/mywin.h
@@ -1,4 +1,14 @@
 #ifndef MYWIN_H
 #define MYWIN_H
 
+#include <QtGui>
+#include <QMainWindow>
+
+class MyWin : public QMainWindow
+{
+    Q_OBJECT
+public:
+    MyWin(const QString& title);
+};
+
 #endif // MYWIN_H
