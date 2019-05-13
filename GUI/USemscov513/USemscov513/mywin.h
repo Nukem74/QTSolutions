95fae2e7c8485c9f7d48b2aba6f9833b12f84618
Nukem74 <Nukem74@gmail.com>
2019-05-13 06:55:28 +0500

    Usemscov 513 start

---
+++ GUI/USemscov513/USemscov513/mywin.h
@@ -0,0 +1,4 @@
+#ifndef MYWIN_H
+#define MYWIN_H
+
+#endif // MYWIN_H
--- GUI/Usemscov510/Usemscov510/mycheckbox.cpp
+++ GUI/Usemscov510/Usemscov510/mycheckbox.cpp
@@ -9,16 +9,17 @@ MyCheckBox::MyCheckBox(const QString& text, QWidget *parent /* = 0 */) : QCheckB
 
 void MyCheckBox::mousePressEvent(QMouseEvent *event)        //перегрузка метода
 {
-    switch(event->button())
+    switch(event->button())                                 //выбор реакции на нажатие различных кнопок мыши
     {
-    case Qt::LeftButton: setCheckState(Qt::Checked);
+    case Qt::LeftButton: setCheckState(Qt::Checked);        //реакция для левой кнопки мыши
         break;
 
-    case Qt::RightButton:setCheckState(Qt::Unchecked);
+    case Qt::RightButton:setCheckState(Qt::Unchecked);      //реакция для правой кнопки мыши
         break;
-    case Qt::MidButton:setCheckState(Qt::PartiallyChecked);
+    case Qt::MidButton:setCheckState(Qt::PartiallyChecked); //реакция для средней кнопки мыши
 
     }
-    event->ignore();
+    event->ignore();                                        //игнорировать событие - не передовать его для дальнейшей обработки родительским элементом(что?)
 
 }
+
