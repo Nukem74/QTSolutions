1d7143b0d86fc043355db8449d5ea65636a88d9f
Nukem74 <Nukem74@gmail.com>
2019-05-15 09:03:40 +0500

    lafore sandbox ch10 horseridefail

--- console/Lafore/SandboxChapter10/SandboxCh10/main.cpp
+++ console/Lafore/SandboxChapter10/SandboxCh10/main.cpp
@@ -1,185 +1,142 @@
 #include <QCoreApplication>
 #include <iostream>
 #include <cstring>
+#include <ctime>
+#include <cstdlib>
+#include <conio.h>
+//ах роберт, если бы ты сразу указал в заголовочных файлах msoftcon - я бы понял что не стоит пытать написать эту программу. Не надо так, Роберт
+
 using namespace std;
 
-const int LEN = 80;             //постоянные
-const int MAX = 40;
+const int CPF = 5;                          //постоянные, кстати адреса постоянных переменных можно хранить только в указателях с модификатором const
+const int maxHorses = 7;
 
-class Stack                     //определение класса
+class track;                                //можно ведь сразу описать класс, а не делать его прототип
+
+class horse
 {
-private:                        //приватные члены
-    char st [ MAX ];
-    int top;
-public:                         //публичные члены
-    Stack()                     //конструктор без аргументов, инициализирующий приватное поле
+private:
+    const track* ptrTrack;
+    const int horse_number;
+    float finish_time;
+    float distance_run;
+public:
+    horse(const int n, const track* ptrT);
+    horse_number(n);
+    ptrTrack(ptrT);
+    distance_run(0.0)
     {
-        top = 0;
+        //empty
     }
-    void push(char var)         //публичный метод с одним аргументом
+    ~horse()
     {
-        st[ ++top ]= var;       //обращение к элементу массива с предварительным инкрементом его адреса
+
     }
-    char pop()                  //публичный метод без аргументов, возвращающий значение элемента приватного массива
-    {
-        return st[ top-- ];
-    }
-    int gettop()                //публичный метод, возвращающий значение приватного поля
-    {
-        return top;
-    }
+    void display_horse(const float elapsed_time);
 };
 
-class express                   //определение класса
+class track
 {
-private:                        //приватные члены
-    Stack s;                    //экземпляр класса
-    char* pStr;                 //указатель на тип char
-    int len;
-public:                         //публичные члены
-    express(char* ptr)          //конструктор с одним аргументом
-    {
-        pStr = ptr;
-        len = strlen(pStr);     //присвоение переменной длины xtuj
-    }
-    void parse();               //прототип метода
-    int solve();                //прототип метода
-
-
+private:
+    horse* hArray[maxHorses];
+    int total_horses;
+    int horse_count;
+    const float track_length;
+    float elapsed_time;
+public:
+    track(float lenT, int nH);
+    ~track();
+    void display_track();
+    void run();
+    float get_track_len() const;
 };
 
-void express::parse()                                   //описание метода с помощью оператора разрешения
+void horse::display_horse(float elapsed_time)
 {
-    char ch;                                            //символ из строки
-    char lastval;                                       //последнее значение
-    char lastop;                                        //последний оператор
-
-    for(int i = 0; i < len; i++)                        //каждый символ в строке
+    set_cursor_pos(1 + int(distance_run * CPF), 2 + horse_number * 2);
+    set_color(static_cast<color>(cBLUE + horse_number));
+    char horse_char = '0' +static_cast<char>(horse_number);
+    putchar( ' ' ); putchar( '\xDB'); putchar(horse_char); putchar('\xDB');
+    if(distance_run < ptrTrack->get_track_len() + 1.0/CPF)
     {
-        ch = pStr[i];                                   //вот этот символ
-        if(ch >= '0' && ch <= '9')                      //так вот, если он от 0 до 9
-            s.push(ch - '0');/**/                       //мы его сохраним
-        else if(ch == '+' || ch == '-' || ch == '*' || ch == '/')//если нет
-        {//и он - один из операторов
-            if(s.gettop() == 1)                         //тогда если это первый оператор
-            {
-                s.push(ch);                             //помещаем его в стек
-            }
-            else                                        //если не первый
-            {
-                lastval = s.pop();                      //получаем предыдущее число
-                lastop = s.pop();                       //и предыдущий к числу оператор
-                if( (ch == '*' || ch == '/' ) && (lastop == '+' || lastop == '-'))
-                {//если сейчас обрабатывается один из */, а предыдущий оператор был один из +-
-                    s.push(lastop);                     //возвращаем в стек последний оператор
-                    s.push(lastval);                    //и следующее за ним число
-                }
-                else                                    //иначе
-                {
-                    switch (lastop)                     //выбираем из операторов тот, что совпадет с содержимым обрабатываемой переменной
-                    {
-                            case '+':
-                        {
-                            s.push(s.pop() + lastval);
-                            break;
-                        }
-                            case '-':
-                        {
-                            s.push(s.pop() - lastval);
-                            break;
-                        }
-                        case '*':
-                        {
-                            s.push(s.pop() * lastval);
-                            break;
-                        }
-                        case '/':
-                        {
-                            s.push(s.pop() / lastval);
-                            break;
-                        }
-                            default:                    //или не совпадет, тогда
-                        {
-                            cout <<"\nNot an operator";
-                            exit(1);                    //угробим прогу
-                        }
-                    }
-                }
-                s.push(ch);                             //и добавляем в стек
-                //очень понятные комменты
-            }
-
-        }
-        else
-        {   //если символ не число и не оператор, тогда
-            cout << "\nNot a symbol";
-            exit(1);                                    //угробим прогу
-        }
-     }
-
-
-  }
-int express::solve()                                    //описание метода с помощью оператора разрешения
-{//крч тут мы проводим вычисления с элементами стека, а почему мы этого не сделаем сразу с элементами строки? не могу понять что мешает
-    char lastval;                                       //предыдущее значение
-
-    while(s.gettop() > 1)                               //пока последний элемент стека больше 1
+        if(rand() % 3)
+            distance_run += 0.2F;
+        finish_time = elapsed_time;
+    }
+    else
     {
-        lastval = s.pop();                              //получаем предыдущее значение
-        switch(s.pop())                                 //получаем предыдущий оператор
+        int mins = int(finish_time) / 60;
+        int secs = int(finish_time) - mins * 60;
+        cout << "Time = " << mins << ":" << secs;
+    }
+}
+
+track::track(float letT, int nH): track_length(lenT),total_horses(nH),horse_count(0), elapsed_time(0.0)
+{
+    init_graphics();
+    total_horses = (total_horses > maxHorses) ? maxHorses : total_horses;
+    for (int i; i < total_horses;i++)
+    {
+        hArray[i] = new horse(horse_count++, this);
+    }
+    time_t aTime;
+    srand(static_cast<unsigned>(time(&aTime)));
+    diplay_track();
+}
+
+track::~track()
+{
+    for(int i = 0; i < total_horses; i++)
+        delete hArray[i];
+}
+
+void track::display_track()
+{
+    clear_screen();
+    for (int j = 0;j <= track_length;j++)
+    {
+        for (int k = 1; k <= total_horses * 2 + 1;k++)
         {
-                case '+':
+            set_cursor_pos(j * CPF + 5, k);
+            if(j == 0 || j == track_length)
             {
-                s.push(s.pop() + lastval);
-                break;
+                cout << '\xDE';
             }
-                case '-':
+            else
             {
-                s.push(s.pop() - lastval);
-                break;
-            }
-            case '*':
-            {
-                s.push(s.pop() * lastval);
-                break;
-            }
-            case '/':
-            {
-                s.push(s.pop() / lastval);
-                break;
-            }
-                default:
-            {
-                cout <<"\nNot an operator";
-                exit(1);
+                cout << '\xB3';
             }
         }
     }
-    return int (s.pop());
+
+}
+
+void track::run()
+{
+    while(!kbhit())
+    {
+        elapsed_time += 1.75;
+
+        for (int i = 0; i < total_horses; i++)
+        {
+            hArray[i]->display_horse(elapsed_time);
+            wait(500);
+        }
+    }
+    getch();
+    cout << endl;
+}
+
+float track::get_track_len() const
+{
+    return track_length;
 }
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    char ans;
-    char string [LEN];
 
-    cout << "\n Enter arithmetical expression like 2+3*4/3-2";
-    cout << "\n Numbers should have only one position \n Do not use space or brackets";
-
-    do
-    {
-        cout << "\nExpression: ";
-        cin >> string;
-        express* eptr = new express(string);
-        eptr->parse();
-        cout << "\nResult: "<< eptr->solve();
-        delete eptr;
-        cout << "another one?(y/n)";
-        cin >> ans;
-    }
-    while(ans == 'y');
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     return a.exec();
 }
