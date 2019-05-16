70ecdc60abc2cc2d7d9fded2594527d69ca9eeb7
Nukem74 <Nukem74@gmail.com>
2019-05-16 08:29:53 +0500

    lafore sandbox ch11 notvirt

--- console/Lafore/SandboxChapter11/SandboxCh11/main.cpp
+++ console/Lafore/SandboxChapter11/SandboxCh11/main.cpp
@@ -1,8 +1,46 @@
 #include <QCoreApplication>
+#include <iostream>
+using namespace std;
 
+class Base
+{
+public:
+    void show()
+    {
+        cout << "Base\n";
+    }
+};
+
+class Derv1 : public Base
+{
+public:
+    void show()
+    {
+        cout << "Dervl\n";
+    }
+};
+
+class Derv2 : public Base
+{
+public:
+    void show()
+    {
+        cout << "Derv2\n";
+    }
+};
+
+////////////////////////////////////////////////////////////
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
+////////////////////////////////////////////////////////////
+    Derv1 dv1;
+    Derv2 dv2;
+    Base* ptr;
+    ptr = &dv1;
+    ptr->show();
+    ptr=&dv2;
+    ptr->show();
+////////////////////////////////////////////////////////////
     return a.exec();
 }
