7af6dd1c3b0c6c44e0136a3687755f558b319d5e
Nukem74 <Nukem74@gmail.com>
2019-05-16 17:11:25 +0500

    lafore sandbox ch11 statfunc

--- console/Lafore/SandboxChapter11/SandboxCh11/main.cpp
+++ console/Lafore/SandboxChapter11/SandboxCh11/main.cpp
@@ -2,34 +2,34 @@
 #include <iostream>
 using namespace std;
 
-class gamma
+class gamma                                 //объявление класса
 {
-private:
-    static int total;
+private:                                    //приватные члены
+    static int total;                       //статическое целочисленное поле
     int id;
-public:
-    gamma()
+public:                                     //публичные члены
+    gamma()                                 //конструктор без аргументов устанавливающй значение приватного поля
     {
         total++;
         id = total;
     }
-    ~gamma()
+    ~gamma()                                //деструктор
     {
         total--;
         cout << "Deleted ID: " << id << endl;
     }
-    static void showtotal()
+    static void showtotal()                 //публичный метод, отображающий содержимое приватного поля
     {
         cout << "Total: " << total << endl;
     }
-    void showid()
+    void showid()                           //публичный метод отображающий содержимое приватного поля
     {
         cout << "ID: " << id << endl;
     }
 
 };
 
-int gamma::total = 0;
+int gamma::total = 0;                       //инициализация приватного поля
 ////////////////////////////////////////////////////////////
 int main(int argc, char *argv[])
 {
