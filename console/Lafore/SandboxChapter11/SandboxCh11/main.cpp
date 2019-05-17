49fca4b8feb98b167c61f00ba07f06d3a616ecc7
Nukem74 <Nukem74@gmail.com>
2019-05-17 13:36:47 +0500

    lafore sandbox ch11 strimem2

--- console/Lafore/SandboxChapter11/SandboxCh11/main.cpp
+++ console/Lafore/SandboxChapter11/SandboxCh11/main.cpp
@@ -3,42 +3,105 @@
 #include <cstring>
 using namespace std;
 
-class alpha                         //объявление класса
+class strCount                      //объявление класса
 {
 private:                            //приватные члены
-    int data;
-public:                             //публичные члены
-    alpha()                         //конструктор без аргументов
+    int count;
+    char* str;
+    friend class String;            //декларация классовой дружбы
+    strCount(char* s)               //приватный конструктор с одним аргументом
     {
-        //empty
+        int length = strlen(s);
+        str = new char[length + 1];
+        strcpy(str, s);
+        count = 1;
     }
-    alpha(int d)                    //конструктор с одним аргументом
+    ~strCount()                     //деструктор
     {
-        data = d;
-    }
-    void display()                  //метод отображающий содержимое приватного поля
-    {
-        cout << "\n" << data;
-    }
-    alpha& operator = (alpha& a)    //перегрузка оператора присваивания с новым типом возвращаемых данных
-    {
-        data = a.data;
-        cout << "\n Assignment operator activated";
-        return *this;               //вернуть копию this alpha
+        delete[] str;
     }
 };
+
+class String                        //объявление класса
+{
+private:                            //приватные члены
+    strCount* psc;
+public:                             //публичные члены
+    String()                        //конструктор без аргументов
+    {
+        psc = new strCount("NULL");
+    }
+    /**/
+    String(char* s)                 //мой конструктор с одним аргументом
+    {
+        //этот конструктор не содержится в примере, тем не менее только с его помощью удается инициализировать переменную s3 в листинге
+        //перегрузка оператора присваивания по прежнему не помогает
+        psc = new strCount(s);
+    }
+    /**/
+    String(String& S)               //конструктор копирования
+    {
+        cout << "\nCopying constructor activated!";
+        psc = S.psc;
+        (psc->count)++;
+    }
+    ~String()                       //деструктор
+    {
+        if(psc->count == 1)
+        {
+            delete psc;
+        }
+        else
+        {
+            (psc->count)--;
+        }
+    }
+    void display()                  //публичный метод отображающий значение приватного поля
+    {
+        cout << psc->str;
+        cout << " (addr=" << psc << ")";
+    }
+    String& operator = (String& S)  //перегрузка оператора присваивания(не корректно)
+    {
+        if(this == &S)              //проверка на присваивание самому себе
+        {
+            return *this;
+        }
+        else
+        {
+            cout << "\nAssignment";
+            if(psc->count == 1)
+            {
+                delete psc;
+            }
+            else
+            {
+                (psc->count)--;
+            }
+            psc = S.psc;
+            (psc->count)++;
+            return *this;
+        }
+    }
+};
+
 
 ////////////////////////////////////////////////////////////
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 ////////////////////////////////////////////////////////////
-    alpha a1(37);
-    alpha a2, a3;
-    a3 = a2 = a1;
-    a1.display();
-    a2.display();
-    a3.display();
+    String s3("\nMy final surrender\nAway from inclusion\nI now await the triumphant\nLeft baggage, train station");
+    cout << "\ns3=";
+    s3.display();
+    String s1, s2;
+    s1 = s2 = s3;
+    cout << "\ns1=";
+    s1.display();
+    cout << "\ns2=";
+    s2.display();
+    cout << "\ns3=";
+    s3.display();
 ////////////////////////////////////////////////////////////
     return a.exec();
 }
