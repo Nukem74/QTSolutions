08b08cc89c323147ee04c85d7b7e0708added119
Nukem74 <Nukem74@gmail.com>
2019-05-17 11:01:27 +0500

    lafore sandbox ch11 assign2

--- console/Lafore/SandboxChapter11/SandboxCh11/main.cpp
+++ console/Lafore/SandboxChapter11/SandboxCh11/main.cpp
@@ -3,15 +3,28 @@
 #include <cstring>
 using namespace std;
 
-class what
+class alpha                         //объявление класса
 {
-private:
-    int alpha;
-public:
-    void tester()
+private:                            //приватные члены
+    int data;
+public:                             //публичные члены
+    alpha()                         //конструктор без аргументов
     {
-        this->alpha = 11;//same as alpha = 11
-        cout << this->alpha;//same as cout << alpha
+        //empty
+    }
+    alpha(int d)                    //конструктор с одним аргументом
+    {
+        data = d;
+    }
+    void display()                  //метод отображающий содержимое приватного поля
+    {
+        cout << "\n" << data;
+    }
+    alpha& operator = (alpha& a)    //перегрузка оператора присваивания с новым типом возвращаемых данных
+    {
+        data = a.data;
+        cout << "\n Assignment operator activated";
+        return *this;               //вернуть копию this alpha
     }
 };
 
@@ -20,9 +33,12 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 ////////////////////////////////////////////////////////////
-    what w;
-    w.tester();
-    cout << endl;
+    alpha a1(37);
+    alpha a2, a3;
+    a3 = a2 = a1;
+    a1.display();
+    a2.display();
+    a3.display();
 ////////////////////////////////////////////////////////////
     return a.exec();
 }
