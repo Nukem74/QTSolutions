bdc00236ea26ce44adedd6f1f40bf14fd322be45
Nukem74 <Nukem74@gmail.com>
2019-06-04 09:09:25 +0500

    Recursion Practice Sandbox

--- console/RecursionPractice/RecursionPractice/main.cpp
+++ console/RecursionPractice/RecursionPractice/main.cpp
@@ -2,23 +2,30 @@
 #include <iostream>
 using namespace std;
 
-//Дана строка, содержащая цифры и английские буквы (большие и маленькие). Найти и вывести количество цифр.
-int counter = 0;
-int recursionNumberCounter(char word[])
+//Дана строка, содержащая только английские буквы (большие и маленькие).
+//Добавить символ '*' (звездочка) между буквами (перед первой буквой и после последней символ '*' добавлять не нужно).
+char* recursionDelimiter(char input[], char output[])
 {
-    if((*word != '\0') && (*word > 47) && (*word < 58))
-        counter++;
-    if(*word != '\0')
-        recursionNumberCounter(++word);
-    return counter;
+    *output = *input;
+
+    if(*(++input) != '\0')
+    {
+        *(++output) = '*';
+        recursionDelimiter(input,++output);
+    }
+
+    return output;
 }
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     char line[1000];
+    char word[1000];
     cin >> line;
-    cout << recursionNumberCounter(line);
+    recursionDelimiter(line, word);
+    for(int i = 0; word[i] != '\0'; i++)
+        cout << word[i];
 
     return a.exec();
 }
