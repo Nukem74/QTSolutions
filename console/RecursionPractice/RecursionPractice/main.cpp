d2539e5efcbafb39ea1ff96e9088885f6cfb8557
Nukem74 <Nukem74@gmail.com>
2019-06-04 07:35:15 +0500

    Recursion Practice Sandbox

--- console/RecursionPractice/RecursionPractice/main.cpp
+++ console/RecursionPractice/RecursionPractice/main.cpp
@@ -2,43 +2,24 @@
 #include <iostream>
 using namespace std;
 
-//Определить можно ли с использованием только операций «прибавить 3» и «прибавить 5»
-//получить из числа 1 число N (N - натуральное, не превышает 10^6).
-//Разумеется, само число 1 получить можно, просто не применяя никаких операций.
-
-bool recursivefunction3(int n , int m = 1)
+//Дана строка, содержащая только десятичные цифры. Найти и вывести наибольшую цифру.
+int maximal = 0;
+int recursionMax(unsigned int number)
 {
-    if(m < n)
+    if(maximal < (number % 10))
+        maximal = number % 10;
+    if(number != 0)
     {
-        m = m + 3;
-        recursivefunction3(n,m);
+        number = number / 10;
+        recursionMax(number);
     }
-    return(n == m);
+    return maximal;
 }
-
-bool recusivefunction5(int n, int m = 1)
-{
-    if(m < n)
-    {
-        m = m + 5;
-        recusivefunction5(n,m);
-    }
-    return(n == m);
-}
-
-
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    int top = 1000000;
-    for (int i = 1; i < top;i++)
-    {
-        if(recursivefunction3(i) && recusivefunction5(i))
-            cout << "YES" << endl;
-        else
-            cout << "NO" << endl;
-    }
-
+    unsigned int sample = 1202340432;
+    cout << recursionMax(sample);
     return a.exec();
 }
