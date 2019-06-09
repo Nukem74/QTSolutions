ce7734b8a78908c3f6e658ca22ac6b82748556b9
Nukem74 <Nukem74@gmail.com>
2019-06-09 10:39:00 +0500

    Recursion Practice

--- console/RecursionPractice1/RecursionPractice1/main.cpp
+++ console/RecursionPractice1/RecursionPractice1/main.cpp
@@ -1,8 +1,23 @@
 #include <QCoreApplication>
+#include <iostream>
+using namespace std;
+
+int recursiveSum(int number, int result)
+{
+    result += number % 10;
+    //cout << result << endl;
+    if(number > 0)
+    {
+        recursiveSum(number / 10, result);
+    }
+    cout << result << endl;
+    return result;
+}
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
+    int sum = 0;
+    cout << recursiveSum(2131, sum);
     return a.exec();
 }
