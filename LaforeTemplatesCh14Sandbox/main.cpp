e5aecc7ca84437d3813e8a0ecf9a0dd634a0af60
Nukem74 <Nukem74@gmail.com>
2019-07-15 14:07:36 +0500

    Lafore Chapter 14 Sandbox:TEMPSTACK

--- LaforeTemplatesCh14Sandbox/main.cpp
+++ LaforeTemplatesCh14Sandbox/main.cpp
@@ -2,32 +2,47 @@
 #include <iostream>
 
 using namespace std;
+const int MAX = 100;
 
-template <class atype>                      //Function template
-int find(atype * array, atype value, int size)  //funcion, that returns integer and geting two template arguments and one integer argument
+template <class Type>                       //Class template
+class Stack
 {
-    for(int i = 0; i < size; i++)
-        if(array[i] == value)
-            return i;
-    return -1;
-}
-char chrArr[] = {1, 3, 5, 9, 11, 13};       //samples
-char ch = 5;
-int intArr[] = {1, 3, 5, 9, 11, 13};
-int in = 6;
-long lonArr[] = {1l, 3L, 5L, 9L, 11L, 13L};
-long lo = 11l;
-double dubArr[] = {1.0, 3.0, 5.0, 9.0, 11.0, 13.0};
-double db = 4.0;
+private:                                    //list of private members
+    Type st[MAX];
+    int top;
+public:                                     //list of public members
+    Stack()                                 //constructor without arguments
+    {
+        top = -1;
+    }
+    void push(Type var)                     //void method with template argument
+    {
+        st[++top] = var;
+    }
+    Type pop()                              //template method with no arguments
+    {
+        return st[top--];
+    }
+};
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     //demo
-    cout << "\n 5 found in chrArr at:" << find(chrArr, ch, 6);
-    cout << "\n 6 found in intArr at:" << find(intArr, in, 6);
-    cout << "\n 11 found in lonArr at:" << find(lonArr, lo, 6);
-    cout << "\n 4.0 found in dubArr at:" << find(dubArr, db, 6);
-    cout << endl;
+    Stack<float> s1;
+    s1.push(1111.1F);
+    s1.push(2222.2F);
+    s1.push(3333.3F);
+    cout << "1: " << s1.pop() << endl;
+    cout << "2: " << s1.pop() << endl;
+    cout << "3: " << s1.pop() << endl;
+
+    Stack<long> s2;
+    s2.push(123123123L);
+    s2.push(234234234L);
+    s2.push(345345345L);
+    cout << "1: " << s2.pop() << endl;
+    cout << "2: " << s2.pop() << endl;
+    cout << "3: " << s2.pop() << endl;
     return a.exec();
 }
