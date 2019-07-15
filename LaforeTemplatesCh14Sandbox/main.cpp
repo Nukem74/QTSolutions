c1a3f1206c688fabaab2f529fa1974d66c189c88
Nukem74 <Nukem74@gmail.com>
2019-07-15 13:41:51 +0500

    Lafore Chapter 14 Sandbox:TEMPFIND

--- LaforeTemplatesCh14Sandbox/main.cpp
+++ LaforeTemplatesCh14Sandbox/main.cpp
@@ -3,28 +3,31 @@
 
 using namespace std;
 
-template <class T>                      //Function template
-T abs(T n)
+template <class atype>                      //Function template
+int find(atype * array, atype value, int size)  //funcion, that returns integer and geting two template arguments and one integer argument
 {
-    return(n < 0) ? -n : n;
+    for(int i = 0; i < size; i++)
+        if(array[i] == value)
+            return i;
+    return -1;
 }
+char chrArr[] = {1, 3, 5, 9, 11, 13};       //samples
+char ch = 5;
+int intArr[] = {1, 3, 5, 9, 11, 13};
+int in = 6;
+long lonArr[] = {1l, 3L, 5L, 9L, 11L, 13L};
+long lo = 11l;
+double dubArr[] = {1.0, 3.0, 5.0, 9.0, 11.0, 13.0};
+double db = 4.0;
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    int int1 = 5;
-    int int2 = -6;
-    long lon1 = 70000L;
-    long lon2 = -80000L;
-    double dub1 = 9.95;
-    double dub2 = - 10.15;
-
-    cout <<  "\nabs(" << int1 << ")=" << abs(int1); //absolute int1
-    cout <<  "\nabs(" << int2 << ")=" << abs(int2); //absolute int2
-    cout <<  "\nabs(" << lon1 << ")=" << abs(lon1); //absolute lon1
-    cout <<  "\nabs(" << lon2 << ")=" << abs(lon2); //absolute lon2
-    cout <<  "\nabs(" << dub1 << ")=" << abs(dub1); //absolute dub1
-    cout <<  "\nabs(" << dub2 << ")=" << abs(dub2); //absolute dub2
+    //demo
+    cout << "\n 5 found in chrArr at:" << find(chrArr, ch, 6);
+    cout << "\n 6 found in intArr at:" << find(intArr, in, 6);
+    cout << "\n 11 found in lonArr at:" << find(lonArr, lo, 6);
+    cout << "\n 4.0 found in dubArr at:" << find(dubArr, db, 6);
     cout << endl;
     return a.exec();
 }
