fd196a4ff72fd57e05748793a57e691695af03a3
Nukem74 <Nukem74@gmail.com>
2019-07-16 14:28:30 +0500

    Lafore Chapter 15 Sandbox:STLTRANSFORM

--- console/LaforeSTLChapter15SANDBOX/LaforeSTLChapter15SANDBOX/main.cpp
+++ console/LaforeSTLChapter15SANDBOX/LaforeSTLChapter15SANDBOX/main.cpp
@@ -1,19 +1,21 @@
 #include <QCoreApplication>
 #include <iostream>
 #include <algorithm>
-#include <string>
+//#include <string>
 using namespace std;
 
-void in_to_cm(double in)            //declaration of function
+double in_to_cm(double in)          //declaration of fucntion
 {
-    cout << (in * 2.54) << ' ';
+    return(in * 2.54);
 }
-
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     double inches[] = {3.5, 6.2, 1.0, 12.75, 4.33};//sample
-    for_each(inches, inches+5, in_to_cm);//call "in_to_cm" for each member of sample array
+    double centi[5];//destinated array
+    transform(inches, inches+5, centi, in_to_cm);//call "transform" to fill destinated array with modified members of sample array
+    for(int i = 0; i < 5; i++)
+        cout << centi[i] << ' ';    //displaying
     cout << endl;
     return a.exec();
 }
