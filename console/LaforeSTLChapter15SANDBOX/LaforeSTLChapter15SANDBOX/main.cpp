6282e3a3d6b860f183f34ae5c79f70a668701f43
Nukem74 <Nukem74@gmail.com>
2019-07-16 13:12:55 +0500

    Lafore Chapter 15 Sandbox:STLCOUNT

--- console/LaforeSTLChapter15SANDBOX/LaforeSTLChapter15SANDBOX/main.cpp
+++ console/LaforeSTLChapter15SANDBOX/LaforeSTLChapter15SANDBOX/main.cpp
@@ -3,12 +3,11 @@
 #include <algorithm>
 using namespace std;
 
-int arr[] = {11,22,33,44,55,66,77,88};                              //declaration of sample array
+int arr[] = {33,22,33,44,33,55,66,77};                      //declaration of sample array
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    int* ptr;
-    ptr = find(arr, arr+8,33);                                      //call "find" to get number of item valued 33
-    cout << "First found entry valued 33 at: " << (ptr-arr) << endl;//displaying result
+    int n = count(arr, arr+8,33);                           //call "count" to get number of items valued 33
+    cout << "Value 33 has " << n << " repeats." << endl;    //displaying result
     return a.exec();
 }
