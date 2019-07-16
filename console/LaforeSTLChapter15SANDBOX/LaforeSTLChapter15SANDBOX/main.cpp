2e7fbee33fa57a6066350576523ed03f068c2d63
Nukem74 <Nukem74@gmail.com>
2019-07-16 13:39:18 +0500

    Lafore Chapter 15 Sandbox:STLMERGE

--- console/LaforeSTLChapter15SANDBOX/LaforeSTLChapter15SANDBOX/main.cpp
+++ console/LaforeSTLChapter15SANDBOX/LaforeSTLChapter15SANDBOX/main.cpp
@@ -3,16 +3,16 @@
 #include <algorithm>
 using namespace std;
 
-int source[] = {11, 44, 33, 11, 22, 33, 11, 22, 44};    //declaration of sample array
-int pattern[] = {11, 22 ,33};                           //declaration of target array
+int src1[] = {2, 3, 4, 6, 8};                   //declaration of samples
+int src2[] = {1, 3, 5};
+int dest[8];
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    int* ptr;
-    ptr = search(source, source+9, pattern, pattern+3); //call "search" to find if there are members of pattern array in source array
-    if(ptr == source+9)                                 //displaying result
-        cout << "No match" << endl;
-    else
-        cout << "Match at " << (ptr-source) << endl;
+    merge(src1, src1+5, src2, src2+3, dest);    //merging sample arrays
+    for(int i = 0; i < 8; i++)
+    {
+        cout << dest[i] << endl;                //displaying result
+    }
     return a.exec();
 }
