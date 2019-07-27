616ab110a53bbf1033b4eb2d341b793a5aaf73cd
Nukem74 <Nukem74@gmail.com>
2019-07-28 00:12:32 +0500

    Lafore Ch15.2 SANDBOX XSTCK2

--- console/LaforeChapter15-2SANDBOX/LaforeChapter15-2SANDBOX/main.cpp
+++ console/LaforeChapter15-2SANDBOX/LaforeChapter15-2SANDBOX/main.cpp
@@ -2,37 +2,37 @@
 #include <iostream>
 using namespace std;
 const int MAX = 3;
-/////////////////////////////////////////////////////
-class Stack
+
+class Stack                             //declaration of a class
 {
-private:
+private:                                //private members
     int st[MAX];
     int top;
-public:
-    class FULL
+public:                                 //public members
+    class FULL                          //declaration of exception class
     {
         //empty
     };
 
-    class EMPTY
+    class EMPTY                         //declaration of exception class
     {
         //empty
     };
 
-    Stack()
+    Stack()                             //constructor without arguments
     {
         top = -1;
     }
-    void push(int var)
+    void push(int var)                  //void method with 1 argument
     {
-        if(top >= MAX-1)
+        if(top >= MAX-1)                //check conditions for throwing exception
             throw FULL();
         st[++top] = var;
     }
 
-    int pop()
+    int pop()                           //int method with no arguments
     {
-        if(top < 0)
+        if(top < 0)                     //check conditions for throwing exception
             throw EMPTY();
         return st[top--];
     }
@@ -42,22 +42,22 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     Stack s1;
-    try
+    try                                 //trying suspicious elements for throwing exceptions
     {
         s1.push(11);
         s1.push(22);
         s1.push(33);
-        //s1.push(44);
+        s1.push(44);
         cout << s1.pop() << endl;
         cout << s1.pop() << endl;
         cout << s1.pop() << endl;
         cout << s1.pop() << endl;
     }
-    catch(Stack::FULL)
+    catch(Stack::FULL)                  //catch exceptions with name FULL
     {
         cout << "Exception! Stack overflow!" << endl;
     }
-    catch(Stack::EMPTY)
+    catch(Stack::EMPTY)                 //catch exceptions with name EMPTY
     {
         cout << "Exception! Stack empty!" << endl;
     }
