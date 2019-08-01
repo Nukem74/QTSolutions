fe5546e4b635ca41d44feb8c70f246277aa93882
Nukem74 <Nukem74@gmail.com>
2019-08-01 11:35:48 +0500

    Lafore Ch15Vector VECTCON

--- console/LaforeCh15Vectors/LaforeCh15Vectors/main.cpp
+++ console/LaforeCh15Vectors/LaforeCh15Vectors/main.cpp
@@ -6,18 +6,19 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    vector<int> v;                      //declaration of a vector
-    v.push_back(10);                    //pushing data to vector's back
-    v.push_back(11);
-    v.push_back(12);
-    v.push_back(13);
-    v[0] = 20;                          //correcting vector's members data
-    v[3] = 23;
+    double arr[] = {1.1, 2.2, 3.3, 4.4 };   //initialising double array
 
-    for(int i = 0; i < v.size(); i++)
+    vector<double> v1(arr, arr+4);          //initialising double vector with array members
+    vector<double> v2(4);                   //defining vector with 4 members
+
+    v1.swap(v2);                            //using .swap() to swap members of vectors
+
+    while(!v2.empty())                      //using .empty() to designate end of cycle
     {
-        cout << v[i] << ' ';            //displaying vector
+        cout << v2.back() << ' ';           //using .back() to display last member of vector
+        v2.pop_back();                      //using .pop_back() to delete last member of vector
     }
+
     cout << endl;
     return a.exec();
 }
