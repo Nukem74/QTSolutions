ad77cc92e00c1fa904c915b9db41328ff3a4e7a5
Nukem74 <Nukem74@gmail.com>
2019-08-08 20:44:51 +0500

    Dictionary: binary search + preins sorting

--- console/Dictionary/Dictionary/main.cpp
+++ console/Dictionary/Dictionary/main.cpp
@@ -20,6 +20,10 @@ class nota
 private:
     std::string key;                                                //private string member, that contains key to data
     TYPE* data;                                                     //private templated pointer, that directs to data object
+    nota()
+    {
+        //empty
+    }
     friend class dictionary<TYPE>;                                  //declaration of class friendship(I hope once i will make here a proper inheritance)
 };
 
@@ -35,29 +39,98 @@ public:
     }
     dictionary(std::string k, TYPE* d)                              //constructor with 2 arguments
     {
-        nota <TYPE> * current = new nota <TYPE>;                    //allocating memory for new member of vector
+        nota <TYPE> * current;
+        try
+        {
+           current = new nota <TYPE>;                               //allocating memory for new member of vector
+        }
+        catch(bad_alloc)                                            //exception for failed allocation
+        {
+            cout << "dictionary(string, TYPE*): bad allocation memory";
+            current = nullptr;
+            delete current;
+        }
         current->key = k;                                           //seting key-value to new member's field
         current->data = d;                                          //setting data-pointer to new member's pointer
         headers.push_back(*current);                                //pushing new member to vector
         delete current;                                             //delete pointer for sure
     }
+
+    /*dictionary(std::string k, TYPE d)
+    {
+                                                                    //here goes constructor that initialises vector with templated object(not a pointer)
+        nota <TYPE> * current = new nota <TYPE>;
+        current->key = k;
+        *current->data = d;
+        headers.push_back(*current);
+        delete current;
+    }*/
     void add(std::string k, TYPE* d)                                //void method with 2 arguments
     {
-        nota <TYPE> * current = new nota <TYPE>;                    //allocating memory for new member of vector
+        nota <TYPE> * current;
+        try
+        {
+            current = new nota <TYPE>;                              //allocating memory for new member of vector
+        }
+        catch(bad_alloc)                                            //exception for failed allocation
+        {
+            cout << ".add(string, TYPE*) dictionary: bad allocation memory";
+            current = nullptr;
+            delete current;
+        }
         current->key = k;                                           //seting key-value to new member's field
         current->data = d;                                          //setting data-pointer to new member's pointer
-        headers.push_back(*current);                                //pushing new member to vector
-        delete current;                                             //delete pointer for sure
-    }
-    void find(string k) const                                       //void method with one argument
-    {
-        for(int i = 0; i < headers.size(); i++)                     //iterating through all vector's members(onceI I organize sorting in .add(string k, TYPE* d), I will use binary search for this method)
+        for(unsigned int i = 0; i < headers.size(); i++)                     //iterating through vector to place new member at distinct place
         {
-            if(k == headers[i].key)                                 //if key is matched
+            if(k <= headers[i].key)                                 //condition for inserting new member
             {
-                cout << headers[i].key << ':' << *(headers[i].data) << endl;//display full record in dictionary
+                headers.insert(headers.begin() + i, *current);      //.insert new member in vector
+                return;                                             //skip method
             }
         }
+        headers.push_back(*current);                                //if condition for placing new member didn't met - place new member in the back of the vector
+        delete current;
+    }
+    void show() const                                               //void method with no arguments
+    {
+        for(unsigned int i = 0; i < headers.size(); i++)            //iterating through all vector's members
+        {
+
+                cout << headers[i].key      << ':'
+                     << *(headers[i].data)  << endl;                //display full record in dictionary
+        }
+    }
+    nota <TYPE> find(string s)                                      //my binary searching prototype
+    {
+        if(headers.begin().key == s)
+            return headers.begin();
+        unsigned int left = 1;
+
+        if(headers.end().key == s)
+            return headers.end();
+        unsigned int right = headers.size() - 1;
+
+        while(left < right)
+        {
+            if(headers[(right - left)/2] == s)
+            {
+                return headers[(right - left)/2];
+            }
+
+            if(headers[(right - left)/2] < s)
+            {
+                right = (right - left)/2;
+                continue;
+            }
+
+            if(headers[(right - left)/2] > s)
+            {
+                left = (right - left)/2;
+                continue;
+            }
+        }
+
+
     }
 
 };
@@ -66,13 +139,17 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     int value = 5;
-    dictionary<int>spec("Aa", &value);
+    dictionary<int>spec("aA", &value);
     int ovalue = 99;
-    spec.add("Ab", &ovalue);
+    spec.add("fb", &ovalue);
     int cvalue = 13;
+    spec.add("aA",&cvalue);
     int dvalue = 301;
-    spec.add("Aa",&cvalue);
-    spec.add("Aa",&dvalue);
-    spec.find("Ab");
+    spec.add("AA",&dvalue);
+    int evalue = 2;
+    spec.add("AA", &evalue);
+    int fvalue = 55114;
+    spec.add("fF", &fvalue);
+    spec.show();
     return a.exec();
 }
